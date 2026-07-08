da13309bf59f6967f516210eb861dc4bb2756d06
Layan Alghymah <layanAlghymah@outlook.sa>
2026-07-08 09:13:10 +0000

    Add task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0
  */
 int main(void)
 {
-/*
- *this is the main function, it prints the line
- *
- */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
