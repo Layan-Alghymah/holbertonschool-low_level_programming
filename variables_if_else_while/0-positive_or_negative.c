a0816b321309047a25fe9928fda0681d12ac1a96
Layan Alghymah <layanAlghymah@outlook.sa>
2026-07-10 22:45:40 +0000

    Fix Betty style for task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+ * main - Prints whether a random number is positive, zero, or negative
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
