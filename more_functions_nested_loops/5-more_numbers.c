5a9563a160d1be476b155744ed4ffd9f24fe6ffc
layan alghymah <layanAlghymah@outlook.sa>
2026-07-11 20:32:38 +0000

    Add task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers from 0 to 14 ten times
+ *
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int line;
+	int n;
+
+	line = 0;
+
+	while (line < 10)
+	{
+		n = 0;
+
+		while (n <= 14)
+		{
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+
+			_putchar((n % 10) + '0');
+			n++;
+		}
+
+		_putchar('\n');
+		line++;
+	}
+}
