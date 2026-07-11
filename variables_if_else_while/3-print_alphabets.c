27efb0b6ea01024bdad1e6d8178799b389afc910
Layan Alghymah <layanAlghymah@outlook.sa>
2026-07-11 00:17:27 +0000

    Add task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+		putchar('\n');
+	return (0);
+}
+
+
