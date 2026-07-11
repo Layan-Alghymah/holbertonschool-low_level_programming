3a03d065ba9d08e09923ecc1fc435f7ffe89d459
layan alghymah <layanAlghymah@outlook.sa>
2026-07-11 19:58:36 +0000

    Add task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ *
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n <= 9)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+
+	_putchar('\n');
+}
