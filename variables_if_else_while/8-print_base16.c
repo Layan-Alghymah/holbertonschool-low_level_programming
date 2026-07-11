f0d991b06e95b2ea3d3513de0a30d5f6d051ec52
Layan Alghymah <layanAlghymah@outlook.sa>
2026-07-11 01:21:48 +0000

    Add task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char digit;
+
+	digit = '0';
+
+	while (digit <= '9')
+	{
+		putchar(digit);
+		digit++;
+	}
+
+	digit = 'a';
+
+	while (digit <= 'f')
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
