39c74e944b9506ab814610105d8820d44c0237a0
layan Alghymah <layanAlghymah@outlook.sa>
2026-07-11 13:34:05 +0000

    Add task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the summed numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
