fcd62161dee775dcffe8a283707fad451f1024a3
Layan Alghymah <layanAlghymah@outlook.sa>
2026-07-11 00:41:46 +0000

    Add task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%d", digit);
+		digit++;
+	}
+	printf("\n');
+
+	return (0);
+}
