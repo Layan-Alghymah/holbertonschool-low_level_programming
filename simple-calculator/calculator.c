604551db06769a58dd6c6b5277ccd39f23057582
Layan Alghymah <layanAlghymah@outlook.sa>
2026-07-18 14:05:03 +0000

    Add task 5

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 	int choice;
-	int a;
-	int b;
+	double a;
+	double b;
 
 	printf("Simple Calculator\n");
 
@@ -26,38 +26,55 @@ int main(void)
 		if (choice == 1)
 		{
 			printf("A: ");
-			scanf("%d", &a);
+			scanf("%lf", &a);
 
 			printf("B: ");
-			scanf("%d", &b);
+			scanf("%lf", &b);
 
-			printf("Result: %d\n", a + b);
+			printf("Result: %g\n", a + b);
 		}
 		else if (choice == 2)
 		{
 			printf("A: ");
-			scanf("%d", &a);
+			scanf("%lf", &a);
 
 			printf("B: ");
-			scanf("%d", &b);
+			scanf("%lf", &b);
 
-			printf("Result: %d\n", a - b);
+			printf("Result: %g\n", a - b);
 		}
 		else if (choice == 3)
 		{
 			printf("A: ");
-			scanf("%d", &a);
+			scanf("%lf", &a);
 
 			printf("B: ");
-			scanf("%d", &b);
+			scanf("%lf", &b);
 
-			printf("Result: %d\n", a * b);
+			printf("Result: %g\n", a * b);
+		}
+		else if (choice == 4)
+		{
+			printf("A: ");
+			scanf("%lf", &a);
+
+			printf("B: ");
+			scanf("%lf", &b);
+
+			if (b == 0)
+			{
+				printf("Error: division by zero\n");
+			}
+			else
+			{
+				printf("Result: %g\n", a / b);
+			}
 		}
 		else if (choice < 0 || choice > 4)
 		{
 			printf("Invalid choice\n");
 		}
-		else if (choice == 0)
+		else
 		{
 			printf("Bye!\n");
 		}
