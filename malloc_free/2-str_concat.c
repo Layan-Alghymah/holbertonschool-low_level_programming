04856688fffee8e05a570b6c8ddf721ced5b9119
layan Alghymah <layanAlghymah@outlook.sa>
2026-07-19 13:07:19 +0000

    Add task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,39 +16,26 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0, len2 = 0, i = 0, j = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 
 	length = malloc(sizeof(*length) * (len1 + len2 + 1));
-
 	if (length == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < len1 ; i++)
-	{
 		length[i] = s1[i];
-	}
 
 	for (j = 0; j < len2; j++)
-	{
 		length[len1 + j] = s2[j];
-	}
+
 	length[len1 + len2] = '\0';
 
 	return (length);
