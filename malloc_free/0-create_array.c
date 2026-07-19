9678a4f0dd81cae0d8b65a30896ba2433d50d190
layan Alghymah <layanAlghymah@outlook.sa>
2026-07-19 11:17:54 +0000

    Add task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Creates an array of chars, and initializes it 
+ * @size : number of letters stored
+ * @c : char to store
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *A;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	A = malloc(sizeof(*A) * size);
+
+	if (A == NULL)
+	{
+		return (NULL);
+	}
+	for(i=0 ; i < size ; i++)
+	{
+		A[i] = c;
+	}
+		return (A);
+}
