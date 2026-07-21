dbc9305bc708b832cf2992ea3c49a2e5a72d9dd9
layan Alghymah <layanAlghymah@outlook.sa>
2026-07-21 17:23:41 +0000

    Add task 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Points to a newly allocated space in memory
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ *
+ * Return: poiter to a string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len_s1, len_s2, i, j;
+	char *m;
+
+	len_s1 = 0;
+	len_s2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len_s1] != '\0')
+		len_s1++;
+
+	while (s2[len_s2] != '\0')
+		len_s2++;
+
+	if (n >= len_s2)
+		n = len_s2;
+
+	m = malloc(sizeof(*m) * (len_s1 + n + 1);
+
+	if (m == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len_s1; i++)
+	{
+		m[i] = s1[i];
+	}
+	for (j = n; j < n; j++)
+	{
+		m[len_s1 + j] = s2[j];
+	}
+		m[len_s1 + n] = '\0';
+
+		return (m);
+}
