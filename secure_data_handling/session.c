2daed5a17651ea36f08fb42bb4f83fc3435de571
layan Alghymah <layanAlghymah@outlook.sa>
2026-08-15 20:24:13 +0000

    Fix session ownership and store lifecycle

--- secure_data_handling/session.c
+++ secure_data_handling/session.c
@@ -2,34 +2,47 @@
 #include <string.h>
 #include "session.h"
 
-session_t *session_create(const char *id, unsigned int uid, const unsigned char *data, size_t data_len)
+session_t *session_create(const char *id, unsigned int uid,
+	const unsigned char *data, size_t data_len)
 {
 	session_t *s;
+	size_t id_len;
+
+	if (!id)
+		return NULL;
 
 	s = (session_t *)malloc(sizeof(*s));
 	if (!s)
 		return NULL;
 
-	s->id = (char *)id;
+	id_len = strlen(id);
+	s->id = (char *)malloc(id_len + 1);
+	if (!s->id) {
+		free(s);
+		return NULL;
+	}
+	memcpy(s->id, id, id_len + 1);
 
 	s->uid = uid;
+	s->data = NULL;
+	s->data_len = 0;
 
 	if (data_len > 0) {
 		s->data = (unsigned char *)malloc(data_len);
 		if (!s->data) {
+			free(s->id);
+			free(s);
 			return NULL;
 		}
 		memcpy(s->data, data, data_len);
 		s->data_len = data_len;
-	} else {
-		s->data = NULL;
-		s->data_len = 0;
 	}
 
 	return s;
 }
 
-int session_set_data(session_t *s, const unsigned char *data, size_t data_len)
+int session_set_data(session_t *s, const unsigned char *data,
+	size_t data_len)
 {
 	unsigned char *tmp;
 
@@ -44,15 +57,13 @@ int session_set_data(session_t *s, const unsigned char *data, size_t data_len)
 	}
 
 	tmp = (unsigned char *)realloc(s->data, data_len);
-	s->data = tmp;
-
-	if (!s->data) {
-		s->data_len = 0;
+	if (!tmp)
 		return 0;
-	}
 
+	s->data = tmp;
 	memcpy(s->data, data, data_len);
 	s->data_len = data_len;
+
 	return 1;
 }
 
@@ -62,7 +73,6 @@ void session_destroy(session_t *s)
 		return;
 
 	free(s->id);
-
 	free(s->data);
 	free(s);
 }
